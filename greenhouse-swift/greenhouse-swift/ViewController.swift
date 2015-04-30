6803425541ef3768e418eeee71a6bfcb0ed24256
Lauris Kruusamae <lauris.kruusamae@lab.mobi>
2015-04-30 14:45:08 +0300

    Create new app with new Xcode and Swift version

--- greenhouse-swift/greenhouse-swift/ViewController.swift
+++ greenhouse-swift/greenhouse-swift/ViewController.swift
@@ -2,14 +2,14 @@
 //  ViewController.swift
 //  greenhouse-swift
 //
-//  Created by lauris on 05/08/14.
-//  Copyright (c) 2014 greenhouse. All rights reserved.
+//  Created by lauris on 30/04/15.
+//  Copyright (c) 2015 greenhouse. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-                            
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- greenhouse-swift/greenhouse-swiftTests/greenhouse_swiftTests.swift
+++ greenhouse-swift/greenhouse-swiftTests/greenhouse_swiftTests.swift
@@ -2,8 +2,8 @@
 //  greenhouse_swiftTests.swift
 //  greenhouse-swiftTests
 //
-//  Created by lauris on 05/08/14.
-//  Copyright (c) 2014 greenhouse. All rights reserved.
+//  Created by lauris on 30/04/15.
+//  Copyright (c) 2015 greenhouse. All rights reserved.
 //
 
 import UIKit
